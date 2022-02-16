fb9df03defe94c0e89d9b28ac4bce5ce9f70f91f
dosgo <dosgo@qq.com>
2022-02-16 11:54:07 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"go-tun2socks/core"
-	"go-tun2socks/dns"
+	"github.com/dosgo/go-tun2socks/core"
+	"github.com/dosgo/go-tun2socks/dns"
 )
 
 func main() {
-	var tunDns="127.0.0.1:53";
-	var socksAddr="127.0.0.1:1080"
+	var tunDns = "127.0.0.1:53"
+	var socksAddr = "127.0.0.1:1080"
 
 	//start local dns server (doh)
-	dns.StartDns(tunDns);
+	dns.StartDns(tunDns)
 
-	core.StartTunDevice("tun0", "10.0.0.2", "255.255.255.0", "10.0.0.1", 1500,socksAddr,tunDns);
+	core.StartTunDevice("tun0", "10.0.0.2", "255.255.255.0", "10.0.0.1", 1500, socksAddr, tunDns)
 }
