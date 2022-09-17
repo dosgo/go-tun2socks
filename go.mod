1eb1e1bcaf99a4cc85e39fa8e6fe6a2a6bed1093
DosGo <dosgo@qq.com>
2022-09-17 23:23:32 +0800

    code finishing

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/dosgo/go-tun2socks
 go 1.18
 
 require (
-	github.com/dosgo/xsocks v0.0.0-20220829135017-7da56ad69d8c
 	github.com/miekg/dns v1.1.50
 	github.com/sensepost/godoh v0.0.0-20210124161331-ae4b9360d5dd
 	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
@@ -15,11 +14,7 @@ require (
 )
 
 require (
-	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/google/btree v1.0.1 // indirect
-	github.com/yijunjun/route-table v0.0.0-20160321072918-9608fc1e2e44 // indirect
-	github.com/yl2chen/cidranger v1.0.2 // indirect
-	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/mod v0.5.1 // indirect
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	golang.org/x/text v0.3.8-0.20211105212822-18b340fc7af2 // indirect
--- tun/nowin.go
+++ tun/nowin.go
@@ -1,57 +1,108 @@
+//go:build !windows
 // +build !windows
 
 package tun
 
 import (
 	"fmt"
-	"github.com/dosgo/xsocks/comm"
 	"github.com/songgao/water"
+	"os/exec"
 	"net"
+	"strings"
+	"strconv"
 	"runtime"
 )
 
+func GetCidrIpRange(cidr string) (string, string) {
+	ip := strings.Split(cidr, "/")[0]
+	ipSegs := strings.Split(ip, ".")
+	maskLen, _ := strconv.Atoi(strings.Split(cidr, "/")[1])
+	seg3MinIp, seg3MaxIp := getIpSeg3Range(ipSegs, maskLen)
+	seg4MinIp, seg4MaxIp := getIpSeg4Range(ipSegs, maskLen)
+	ipPrefix := ipSegs[0] + "." + ipSegs[1] + "."
 
-/*windows linux mac use tun dev*/
-func RegTunDev(tunDevice string,tunAddr string,tunMask string,tunGW string,tunDNS string)(*water.Interface,error){
-	if len(tunDevice)==0 {
-		tunDevice="tun0";
-	}
-	if len(tunAddr)==0 {
-		tunAddr="10.0.0.2";
-	}
-	if len(tunMask)==0 {
-		tunMask="255.255.255.0";
-	}
-	if len(tunGW)==0 {
-		tunGW="10.0.0.1";
-	}
-	if len(tunDNS)==0 {
-		tunDNS="114.114.114.114";
-	}
+	return ipPrefix + strconv.Itoa(seg3MinIp) + "." + strconv.Itoa(seg4MinIp),
+		ipPrefix + strconv.Itoa(seg3MaxIp) + "." + strconv.Itoa(seg4MaxIp)
+}
 
-	config := comm.GetWaterConf(tunAddr,tunMask);
-	ifce, err := water.New(config)
-	if err != nil {
-		fmt.Println("start tun err:", err)
-		return nil,err;
+//得到第四段IP的区间（第一片段.第二片段.第三片段.第四片段）
+func getIpSeg4Range(ipSegs []string, maskLen int) (int, int) {
+	ipSeg, _ := strconv.Atoi(ipSegs[3])
+	segMinIp, segMaxIp := getIpSegRange(uint8(ipSeg), uint8(32-maskLen))
+	return segMinIp + 1, segMaxIp
+}
+
+//得到第三段IP的区间（第一片段.第二片段.第三片段.第四片段）
+func getIpSeg3Range(ipSegs []string, maskLen int) (int, int) {
+	if maskLen > 24 {
+		segIp, _ := strconv.Atoi(ipSegs[2])
+		return segIp, segIp
 	}
-	//set ifco conf
-	if runtime.GOOS=="windows" {
-		comm.CmdHide("netsh", "interface","ip","set","address","name="+ifce.Name(),"source=static","addr="+tunAddr,"mask="+tunMask,"gateway=none").Run();
-	}else if runtime.GOOS=="linux"{
-		//sudo ip addr add 10.1.0.10/24 dev O_O
-		masks:=net.ParseIP(tunMask).To4();
-		maskAddr:=net.IPNet{IP: net.ParseIP(tunAddr), Mask: net.IPv4Mask(masks[0], masks[1], masks[2], masks[3] )}
-		comm.CmdHide("ip", "addr","add",maskAddr.String(),"dev",ifce.Name()).Run();
-		comm.CmdHide("ip", "link","set","dev",ifce.Name(),"up").Run();
-	}else if runtime.GOOS=="darwin"{
-		//ifconfig utun2 10.1.0.10 10.1.0.20 up
-		masks:=net.ParseIP(tunMask).To4();
-		maskAddr:=net.IPNet{IP: net.ParseIP(tunAddr), Mask: net.IPv4Mask(masks[0], masks[1], masks[2], masks[3] )}
-		ipMin,ipMax:=comm.GetCidrIpRange(maskAddr.String());
-		comm.CmdHide("ifconfig", "utun2",ipMin,ipMax,"up").Run();
-	}
-	return ifce,nil;
+	ipSeg, _ := strconv.Atoi(ipSegs[2])
+	return getIpSegRange(uint8(ipSeg), uint8(24-maskLen))
 }
 
 
+//根据用户输入的基础IP地址和CIDR掩码计算一个IP片段的区间
+func getIpSegRange(userSegIp, offset uint8) (int, int) {
+	var ipSegMax uint8 = 255
+	netSegIp := ipSegMax << offset
+	segMinIp := netSegIp & userSegIp
+	segMaxIp := userSegIp&(255<<offset) | ^(255 << offset)
+	return int(segMinIp), int(segMaxIp)
+}
+
+func GetWaterConf(tunAddr string, tunMask string) water.Config {
+	config := water.Config{
+		DeviceType: water.TUN,
+	}
+	config.Name = "tun2"
+	return config
+}
+
+/*windows linux mac use tun dev*/
+func RegTunDev(tunDevice string, tunAddr string, tunMask string, tunGW string, tunDNS string) (*water.Interface, error) {
+	if len(tunDevice) == 0 {
+		tunDevice = "tun0"
+	}
+	if len(tunAddr) == 0 {
+		tunAddr = "10.0.0.2"
+	}
+	if len(tunMask) == 0 {
+		tunMask = "255.255.255.0"
+	}
+	if len(tunGW) == 0 {
+		tunGW = "10.0.0.1"
+	}
+	if len(tunDNS) == 0 {
+		tunDNS = "114.114.114.114"
+	}
+
+	config := GetWaterConf(tunAddr, tunMask)
+	ifce, err := water.New(config)
+	if err != nil {
+		fmt.Println("start tun err:", err)
+		return nil, err
+	}
+	//set ifco conf
+	if runtime.GOOS == "windows" {
+		CmdHide("netsh", "interface", "ip", "set", "address", "name="+ifce.Name(), "source=static", "addr="+tunAddr, "mask="+tunMask, "gateway=none").Run()
+	} else if runtime.GOOS == "linux" {
+		//sudo ip addr add 10.1.0.10/24 dev O_O
+		masks := net.ParseIP(tunMask).To4()
+		maskAddr := net.IPNet{IP: net.ParseIP(tunAddr), Mask: net.IPv4Mask(masks[0], masks[1], masks[2], masks[3])}
+		CmdHide("ip", "addr", "add", maskAddr.String(), "dev", ifce.Name()).Run()
+		CmdHide("ip", "link", "set", "dev", ifce.Name(), "up").Run()
+	} else if runtime.GOOS == "darwin" {
+		//ifconfig utun2 10.1.0.10 10.1.0.20 up
+		masks := net.ParseIP(tunMask).To4()
+		maskAddr := net.IPNet{IP: net.ParseIP(tunAddr), Mask: net.IPv4Mask(masks[0], masks[1], masks[2], masks[3])}
+		ipMin, ipMax := GetCidrIpRange(maskAddr.String())
+		CmdHide("ifconfig", "utun2", ipMin, ipMax, "up").Run()
+	}
+	return ifce, nil
+}
+
+func CmdHide(name string, arg ...string) *exec.Cmd {
+	return exec.Command(name, arg...)
+}
